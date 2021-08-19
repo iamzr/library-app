6e0d5fec07fd23a79c3147c20d4e1a42a92d51ea
Zia Rehman <iamziarehman@gmail.com>
2021-08-20 00:19:32 +0100

    Made cards for each book, set up ui for new book form

--- book.js
+++ book.js
@@ -1,3 +1,30 @@
+let myLibrary = [{
+    title: "Harry Potter",
+    author: "J.K. Rowling",
+    pages: "400",
+    isRead: false,
+}, {
+    title: "Lost",
+    author: "Some guy",
+    pages: "200",
+    isRead: true
+}, {
+    title: "Lost",
+    author: "Some guy",
+    pages: "200",
+    isRead: true
+}, {
+    title: "Lost",
+    author: "Some guy",
+    pages: "200",
+    isRead: true
+}, {
+    title: "Lost",
+    author: "Some guy",
+    pages: "200",
+    isRead: true
+}];
+
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
@@ -9,3 +36,48 @@ function Book(title, author, pages, isRead) {
     }
 }
 
+function addBookToLibrary(userBook) {
+    myLibrary.push(userBook)
+}
+
+function displayBooks (Library) {
+    Library.forEach(book => {
+        bookContainer = document.getElementById("book-container")
+        p = document.createElement("p")
+        div = document.createElement("div")
+        div.classList = "card"
+
+        div.innerHTML = `
+        <p>Title: ${book.title}</p>
+        <p>Author: ${book.author}</p>
+        <p>Pages: ${book.pages}</p>
+        <button class="read-button">${book.isRead == true ? "Read" : "Unread"}</button>
+        `;
+        bookContainer.appendChild(div)
+    })
+}
+
+displayBooks(myLibrary);
+
+// New book form javascript
+const newBookButton = document.getElementById("new-book-button");
+const formContainer = document.getElementById("new-book-form-container");
+const closeButton = document.getElementById("close-button")
+
+// New book button opens form
+newBookButton.addEventListener("click", () => {
+    formContainer.style.display = "block";
+}
+)
+
+// Close buttons closes form
+closeButton.addEventListener("click", () => {
+    formContainer.style.display = "none";
+})
+
+// Clicking outside the form closes it
+window.addEventListener("click", (e) => {
+    if (e.target == formContainer) {
+        formContainer.style.display = "none";
+    }
+})
