3c5f134bb8b76e3d3ed34e42738c592c3f8aede5
Zia Rehman <iamziarehman@gmail.com>
2021-08-21 16:20:39 +0100

    Added reset-library button, started using localStorage

--- book.js
+++ book.js
@@ -1,4 +1,4 @@
-let myLibrary = [{
+let myLibraryDefault = [{
     title: "Harry Potter",
     author: "J.K. Rowling",
     pages: "400",
@@ -25,6 +25,49 @@ let myLibrary = [{
     isRead: true
 }];
 
+//Local storage check
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+// Testing for local storage is populated
+if (storageAvailable("localStorage")) {
+    if (!localStorage.getItem("myLibrary")) {
+        localStorage.setItem("myLibrary", JSON.stringify(myLibraryDefault))
+    }
+    myLibrary = JSON.parse(localStorage.getItem("myLibrary"))
+} else {
+    alert("Local storage is not available, please enable")
+}
+
+
+displayBooks(myLibrary);
+
+
+//// All functions 
+
+// BOOK function
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
@@ -37,10 +80,14 @@ function Book(title, author, pages, isRead) {
 }
 
 function addBookToLibrary(userBook) {
-    myLibrary.push(userBook)
+    myLibrary.push(userBook);
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary))
+    myLibrary = JSON.parse(localStorage.getItem("myLibrary"))
 }
 
+    
 function displayBooks (Library) {
+    document.getElementById("book-container").innerHTML = "";
     Library.forEach(book => {
         bookContainer = document.getElementById("book-container")
         p = document.createElement("p")
@@ -58,7 +105,8 @@ function displayBooks (Library) {
     })
 }
 
-displayBooks(myLibrary);
+
+// FORM FUNCTIONS 
 
 // New book form javascript
 const newBookButton = document.getElementById("new-book-button");
@@ -82,6 +130,26 @@ window.addEventListener("click", (e) => {
     }
 })
 
+// Submitting form adds new card
+// const submitButton = document.getElementById("submit-button");
+// submitButton.addEventListener("click")
+
+const form = document.getElementById("new-book-form");
+
+form.addEventListener("submit", () => {
+    let titleInput = document.getElementById("title-input").value;
+    let authorInput = document.getElementById("author-input").value;
+    let pageInput = document.getElementById("pages-input").value;
+    let readInput = document.getElementById("read-input").value;
+
+    newBook = Book(titleInput, authorInput, pageInput, readInput);
+    addBookToLibrary(newBook);
+})
+
+
+
+// CARD FUNCTIONS
+
 // Clicking read button
 const isReadButtons = document.querySelectorAll(".read-button")
 
@@ -104,6 +172,15 @@ const removeButtons = document.querySelectorAll(".remove-button");
 removeButtons.forEach(removeButton => {
     removeButton.addEventListener("click", (e) => {
         removeButton.parentNode.remove();
-        
     })
+})
+
+// Clear library
+const resetLibraryButton = document.getElementById("reset-library")
+
+resetLibraryButton.addEventListener("click", () => {
+    localStorage.removeItem("myLibrary")
+    localStorage.setItem("myLibrary", JSON.stringify(myLibraryDefault))
+    myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+    displayBooks(myLibrary)
 })
